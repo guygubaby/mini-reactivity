3a7ea09356f2cffd9c6999b9de0fe7aab05d64d5
guygubaby <guygubaby@gmail.com>
2021-07-28 10:23:44 +0800

    fix: fix file structure

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { effect, reactive } from '@vue/reactivity'
-
-const state = reactive({
-  count: 0,
-})
-
-effect(() => {
-  console.log('effect', state)
-})
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,27 @@
 import { queueJob } from './scheduler'
 
+type ReactivityTarget = Record<string| symbol, any>
+
 type Effect = () => void
-type Deps = Set<Effect>
-type KeyToDepMap = Map<string| symbol, Deps>
-type EffectMap = WeakMap<any, KeyToDepMap>
+type Effects = Set<Effect>
+type KeyToEffectsMap = Map<string| symbol, Effects>
+type EffectMap = WeakMap<any, KeyToEffectsMap>
 
 const runningEffects: Effect[] = []
 const effectMap: EffectMap = new WeakMap()
 
-const track = <T extends Record<string| symbol, any>>(target: T, property: string | symbol) => {
+const track = <T extends ReactivityTarget>(target: T, property: string | symbol) => {
   const effects = runningEffects.slice()
-  const oldDeps = effectMap.get(target)?.get(property) || new Set<Effect>()
+  const oldEffects = effectMap.get(target)?.get(property) || new Set<Effect>()
 
-  const deps: Deps = new Set([...oldDeps, ...effects])
-  const depMap: KeyToDepMap = effectMap.get(target) || new Map()
-  depMap.set(property, deps)
+  const allEffects: Effects = new Set([...oldEffects, ...effects])
+
+  const depMap: KeyToEffectsMap = effectMap.get(target) || new Map()
+  depMap.set(property, allEffects)
   effectMap.set(target, depMap)
 }
 
-const trigger = <T extends Record<string| symbol, any>>(target: T, property: string | symbol) => {
+const trigger = <T extends ReactivityTarget>(target: T, property: string | symbol) => {
   const effects = effectMap.get(target)?.get(property)
   if (effects) {
     effects.forEach((effect) => {
@@ -27,16 +30,15 @@ const trigger = <T extends Record<string| symbol, any>>(target: T, property: str
   }
 }
 
-export const reactive = <T extends Record<string| symbol, any>>(target: T) => {
+export const reactive = <T extends ReactivityTarget>(target: T) => {
   const proxy = new Proxy(target, {
     get(target, property, receiver) {
       track(target, property)
       return Reflect.get(target, property, receiver)
     },
     set(target, property, value) {
-      const ret = Reflect.set(target, property, value)
       trigger(target, property)
-      return ret
+      return Reflect.set(target, property, value)
     },
   })
   return proxy
