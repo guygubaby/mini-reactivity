d15dbb412ba1175d9239cf0af843edfd25f53c41
guygubaby <guygubaby@gmail.com>
2021-07-30 14:50:43 +0800

    feat: add more style module

--- src/demos/demo03.ts
+++ src/demos/demo03.ts
@@ -1,4 +1,4 @@
-import { h, init } from 'snabbdom'
+import { h, init, styleModule } from 'snabbdom'
 import { reactive, createEffect } from '../index'
 
 const root = document.getElementById('app')!
@@ -8,14 +8,30 @@ const state = reactive({
   count: 0,
 })
 
-const patch = init([])
+const patch = init([
+  styleModule,
+])
 
-let vnode = h('span', null, state.count)
+let vnode = h('div', null, [
+  h('span', {
+    style: {
+      color: 'red',
+    },
+  }, state.count),
+])
 
 patch(root, vnode)
 
 createEffect(() => {
-  const nextVnode = h('span', null, state.count)
+  const nextVnode = h('div', null, [
+    h('span', null, state.count),
+    h('div', null, null),
+    h('span', {
+      style: {
+        color: state.count % 2 === 0 ? 'red' : 'pink',
+      },
+    }, state.count + 1),
+  ])
   patch(vnode, nextVnode)
   vnode = nextVnode
 })
